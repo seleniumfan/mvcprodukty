c29cede7a67d32a0e62e1af35391b4654005a7ec
Tomasz Iwanicki <tiko35@o2.pl>
2023-06-29 16:10:08 +0200

    Initial commit

---
+++ src/main/java/com/ti/mvcprodukty/MvcProduktyApplication.java
@@ -0,0 +1,13 @@
+package com.ti.mvcprodukty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProduktyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcProduktyApplication.class, args);
+    }
+
+}
